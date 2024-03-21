561382abaf2a5104cd7f589c0cc4650368046435
ElidTG <torresgonzalezelidailton@gmail.com>
2024-03-21 09:51:10 -0600

    Update tabla_routes.js

--- routes/tabla/tabla_routes.js
+++ routes/tabla/tabla_routes.js
@@ -11,9 +11,9 @@ app.get('/buscartabla/:ruta', async(req, res) => {
     })
 });
 
-app.get('/gettabla/:rpu', async(req, res) => {
-    const rpu = req.params.rpu;
-    const reg = await registros.findOne({ Rpu: rpu });
+app.get('/gettabla/:NumeroM', async(req, res) => {
+    const NumeroM = req.params.NumeroM;
+    const reg = await registros.findOne({ NumeroM: NumeroM});
 
     if (reg) {
         return res.status(200).json({
@@ -26,7 +26,32 @@ app.get('/gettabla/:rpu', async(req, res) => {
             message: 'Registro no encontrado'
         });
     }
-});
+}); 
+/*app.get('/1gettabla/:parametro', async (req, res) => {
+    const parametro = req.params.parametro;
+    let reg;
+
+    // Check if the parametro is a valid Rpu
+    if (/^\d+$/.test(parametro)) {
+        reg = await registros.findOne({ NumeroM: parametro });
+    } else {
+        // If it's not an Rpu, assume it's a NumeroM and search by that
+        reg = await registros.findOne({ Rpu: parametro });
+    }
+
+    if (reg) {
+        return res.status(200).json({
+            ok: true,
+            registro: reg
+        });
+    } else {
+        return res.status(404).json({
+            ok: false,
+            message: 'Registro no encontrado'
+        });
+    }
+});*/
+
 app.post('/patabla', (req, res) => {
     let body = req.body;
     let tabla = new registros({ 
@@ -52,17 +77,17 @@ app.post('/patabla', (req, res) => {
     
 });
 
-app.post('/actualizartabla/:rpu', async (req, res) => {
+app.post('/actualizartabla/:NumeroM', async (req, res) => {
     try {
-        const rpuBuscar = req.params.rpu;
+        const numeromBuscar = req.params.NumeroM;
         const body = req.body;
-        console.log(rpuBuscar);
+        console.log(numeromBuscar);
         console.log(body);
-        // Encuentra el registro por RPU y lo actualiza con los datos del cuerpo (body).
-        const tablaModificar = await registros.findOneAndUpdate({ Rpu: rpuBuscar }, body, { new: true });
+        // Encuentra el registro por Numero de Medidor y lo actualiza con los datos del cuerpo (body).
+        const tablaModificar = await registros.findOneAndUpdate({ NumeroM: numeromBuscar }, body, { new: true });
         console.log(tablaModificar);
         if (tablaModificar) {
-            console.log('Modifica algo?');
+            console.log('MODIFICAR');
             return res.status(200).json({
                 ok: true,
                 tabla: tablaModificar
